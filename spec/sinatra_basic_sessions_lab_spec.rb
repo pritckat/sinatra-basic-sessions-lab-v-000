689080d265359e362cef1553d55b3aec4be84726
Katrina Pritchard <pritckat@gmail.com>
2018-09-13 18:00:02 +0000

    automatically backed up by learn

--- spec/sinatra_basic_sessions_lab_spec.rb
+++ spec/sinatra_basic_sessions_lab_spec.rb
@@ -14,25 +14,25 @@ describe App do
     end
   end
 
-  # describe 'POST /checkout' do
-  #
-  #   it "displays the shopping cart contents" do
-  #     visit '/'
-  #
-  #     fill_in(:item, :with => "socks")
-  #     click_button "submit"
-  #     expect(page).to have_text("socks")
-  #   end
-  #
-  #   it "sets the params in the session hash" do
-  #     params = {
-  #       :item => "socks"
-  #     }
-  #     post '/checkout', params
-  #     expect(session.has_key?(:item)).to eq(true)
-  #     expect(session[:item]).to eq(params[:item])
-  #   end
-  # end
+  describe 'POST /checkout' do
+  
+    it "displays the shopping cart contents" do
+      visit '/'
+  
+      fill_in(:item, :with => "socks")
+      click_button "submit"
+      expect(page).to have_text("socks")
+    end
+  
+    it "sets the params in the session hash" do
+      params = {
+        :item => "socks"
+      }
+      post '/checkout', params
+      expect(session.has_key?(:item)).to eq(true)
+      expect(session[:item]).to eq(params[:item])
+    end
+  end
 
 
 end
